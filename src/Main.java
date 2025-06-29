bc33ae8657d2ce520cf32868163b9f542cdf40ea
MaryPV <fornewstudies@gmail.com>
2025-06-29 14:24:51 +0500

    Initial commit2 finish

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,22 @@ public class Main {
         for (int i = 0; i < names.length; i++) {
             System.out.println("Имя " + names[i] + " Возраст " + ages[i]);
         }*/
-        Person sarah = new Person("Sarah", 30);
+        /*Person sarah = new Person("Sarah", 30);
         System.out.println("sarah.name = " + sarah.getName());
         System.out.println("sarah.age = " + sarah.getAge());
         sarah.setAge(31);
         System.out.println("sarah.getAge() = " + sarah.getAge());
         Person john = new Person("John", 13);
-        System.out.println("john = " + john.getName());
+        System.out.println("john = " + john.getName());*/
+        PersonService personService = new PersonService();
+        Person sarah = new Person("Sarah", 30);
+        if (personService.isAdult(sarah)) {
+            System.out.println("Иди в бар");
+        } else {
+            System.out.println("Иди в школу");
+        }
+        System.out.println(sarah);
+        sarah.increaseAge(1);
+        System.out.println(sarah);
     }
 }
